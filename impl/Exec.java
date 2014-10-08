1c4ec551f6da612830ecaf17c34b08d878cbd178
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2014-10-07 22:33:19 -0300

    main class

---
+++ impl/Exec.java
@@ -0,0 +1,16 @@
+package impl;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import impl.SemanticCrawlerImpl;
+
+public class Exec {
+	
+	public static final String INITIAL_URI = "http://dbpedia.org/resource/Roger_Federer";
+	
+	public static void main(String[] args) {
+		Model model = ModelFactory.createDefaultModel();
+		SemanticCrawlerImpl crawler = new SemanticCrawlerImpl();
+		crawler.search(model, INITIAL_URI);	
+	}
+}
