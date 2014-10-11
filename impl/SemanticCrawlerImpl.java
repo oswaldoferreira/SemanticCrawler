5f9eb2b7093209da4df8568567e1d7a32b2895ba
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2014-10-11 14:31:19 -0300

    changing the main query (fixing)

--- impl/SemanticCrawlerImpl.java
+++ impl/SemanticCrawlerImpl.java
@@ -1,12 +1,18 @@
 package impl;
 
+import com.github.jsonldjava.core.RDFDataset.Literal;
+import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSetFactory;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Model;
 //import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 
 import crawler.SemanticCrawler;
 
@@ -15,14 +21,16 @@ public class SemanticCrawlerImpl implements SemanticCrawler {
 	public void search(Model graph, String resourceURI) {
 		graph.read(resourceURI);
 		
-		// Create a new query
-		String queryString = 
-			"SELECT ?url ?property ?object" +
-			"WHERE {" +
-			"      <"+ resourceURI +"> "+ " ?property ?object . " + 
-			"      }";
-	    
-	    Query query = QueryFactory.create(queryString);
+		ParameterizedSparqlString queryString = new ParameterizedSparqlString( "" +
+                "select ?uri ?property ?resource where {\n" +
+                "  <"+resourceURI+"> ?property ?resource\n" +
+                "}" );
+
+        System.out.println( queryString );
+
+        QueryExecution exec = QueryExecutionFactory.sparqlService( "http://dbpedia.org/sparql", queryString.asQuery() );
+        
+        com.hp.hpl.jena.query.ResultSet results = ResultSetFactory.copyResults( exec.execSelect() );
 
 	    System.out.println("----------------------");
 
@@ -30,14 +38,14 @@ public class SemanticCrawlerImpl implements SemanticCrawler {
 
 	    System.out.println("----------------------");
 	   
-	    // Execute the query and obtain results
-	    QueryExecution qe = QueryExecutionFactory.create(query, graph);
-	    com.hp.hpl.jena.query.ResultSet results =  qe.execSelect();
-
-	    // Output query results    
-	    ResultSetFormatter.out(System.out, results, query);
-
-	    qe.close();
+	    while ( results.hasNext() ) {
+            // As RobV pointed out, don't use the `?` in the variable
+            // name here. Use *just* the name of the variable.
+            System.out.println( results.next().get( "resource" ));
+        }
+	    
+	    // A simpler way of printing the results.
+        ResultSetFormatter.out( results );
 	}
 }
 
