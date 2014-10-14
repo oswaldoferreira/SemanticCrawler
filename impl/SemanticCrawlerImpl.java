7513ad7e05bd84aef0d04d05cd8d02a635f01f95
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2014-10-14 13:55:34 -0300

    recursively navigating through dereferenced uris

--- impl/SemanticCrawlerImpl.java
+++ impl/SemanticCrawlerImpl.java
@@ -8,46 +8,45 @@ import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.vocabulary.OWL;
 
 import crawler.SemanticCrawler;
+
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SemanticCrawlerImpl implements SemanticCrawler {
+	Set<RDFNode> visited = new HashSet<RDFNode>();
+	
 	public void search(Model graph, String resourceURI) {
-//		CharsetEncoder enc = Charset.forName("ISO‐8859‐1").newEncoder();
-		
 		graph.read(resourceURI);
 		
 		System.out.println("Following instances with owl:sameAs property");
 		StmtIterator statements = graph.listStatements((Resource) null, OWL.sameAs, (RDFNode) null);
 		
 		while ( statements.hasNext() ) {
-			Statement statement = statements.nextStatement();
-			Resource subject = statement.getSubject();
-			
-			if ( subject.isAnon() ) {
-				System.out.print(" ("+ subject.getId() +" )");
-			} else {
-				String subjectURI = subject.getURI();
-				
-//				if (enc.canEncode(subjectURI)) {
-				System.out.println(" ("+ subjectURI +") ");
-//				}
-			}
-			
-			System.out.print(" OWL.sameAs ");
-			
-			Resource object = (Resource) (statement.getObject());
-			
-			if (object.isAnon()) {
-				System.out.print(" ("+ object +") ");
-			} else if (object.isLiteral()) {
-				System.out.print(" ("+ object.toString() +") ");
-			} else if (object.isResource() ) {
-				System.out.print(" ("+ object.getURI() +") ");
-			}
-			
+			rdfDFS( statements.next().getObject(), visited, "");	
+		}
+		
+		System.out.println("------ VISITADOS ("+ visited.size() +") ------");
+		
+		for (RDFNode node : visited) {
+			System.out.println(node);
 		}
 	}
+	
+	public static void rdfDFS( RDFNode node, Set<RDFNode> visited, String prefix) {
+        if ( !visited.contains( node )) {
+            visited.add( node );
+            
+			if ( node.isResource() ) {
+	            StmtIterator stmts = node.asResource().listProperties( OWL.sameAs );
+	            while ( stmts.hasNext() ) {
+	                Statement stmt = stmts.next();
+	                rdfDFS( stmt.getObject(), visited, node + " =[" + stmt.getPredicate() + "]=> " );
+	            }
+            }
+        }
+    }
 }
 
 
